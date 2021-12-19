caeacd32b5d7be02b7ea0e045fc12ae9c0c0ffd1
sgnovo8 <88598099+sgnovo8@users.noreply.github.com>
2021-12-18 20:15:27 -0500

    Update multiSender.sol

--- multiSender.sol
+++ multiSender.sol
@@ -1,5 +1,6 @@
 
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.3;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -8,6 +9,7 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 
 //no variables?
+// review openzellelin code
 
 
 
